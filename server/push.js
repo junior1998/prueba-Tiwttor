502cd22e5d78e7dc70b439aea7ad529c24d938fd
Yunior <juniorliban@hotmail.com>
2018-10-24 11:29:07 -0400

    prueba de notificaciones push

---
+++ server/push.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+
+const urlsafeBase64 = require('urlsafe-base64');
+const vapid = require('./vapid.json');
+
+const suscripciones = require('./subs-db.json');
+
+const webpush = require('web-push');
+
+webpush.setVapidDetails(
+    'mailto:example@yourdomain.org',
+    vapid.publicKey,
+    vapid.privateKey
+);
+
+
+
+module.exports.getKey = () => {
+    return urlsafeBase64.decode(vapid.publicKey);
+};
+
+module.exports.addSubscription = (suscripcion) => {
+
+    suscripciones.push(suscripcion);
+
+    fs.writeFileSync(`${ __dirname }/subs-db.json`, JSON.stringify(suscripciones))
+
+};
+
+module.exports.sendPush = (post) => {
+
+    suscripciones.forEach((suscripcion, i) => {
+
+        webpush.sendNotification(suscripcion, JSON.stringify(post));
+
+    });
+
+
+}
---
+++ server/routes.js
@@ -0,0 +1,82 @@
+ // Routes.js - Módulo de rutas
+ const express = require('express');
+ const router = express.Router();
+ const push = require('./push');
+
+
+ const mensajes = [
+
+     {
+         _id: 'XXX',
+         user: 'spiderman',
+         mensaje: 'Hola Mundo'
+     }
+
+ ];
+
+
+ // Get mensajes
+ router.get('/', function(req, res) {
+     // res.json('Obteniendo mensajes');
+     res.json(mensajes);
+ });
+
+
+ // Post mensaje
+ router.post('/', function(req, res) {
+
+     const mensaje = {
+         mensaje: req.body.mensaje,
+         user: req.body.user
+     };
+
+     mensajes.push(mensaje);
+
+     //  console.log(mensajes);
+
+
+     res.json({
+         ok: true,
+         mensaje
+     });
+ });
+
+
+ // Almacenar la suscripcion
+ router.post('/subscribe', (req, res) => {
+     const suscripcion = req.body;
+
+     push.addSubscription(suscripcion);
+
+     res.json('subscribe');
+
+ })
+
+
+ // Almacenar la inscripcion
+ router.get('/key', (req, res) => {
+
+     const key = push.getKey()
+
+     res.send(key);
+
+ })
+
+ // almacenar la subscripcion
+ router.post('/push', (req, res) => {
+
+     const post = {
+         titulo: req.body.titulo,
+         cuerpo: req.body.cuerpo,
+         usuario: req.body.usuario
+     };
+
+     push.sendPush(post);
+
+     res.json(post);
+
+ })
+
+
+
+ module.exports = router;
